ebe9decde2d4544c43e49f9605941f0f7aef4eb9
Stefanie Melo <stefanietmelo@gmail.com>
2018-06-28 01:03:07 -0300

    Run eject to gain access to webpack file

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -1,9 +1,8 @@
 export const REQUEST_REVIEWS  = 'REQUEST_REVIEWS';
 export const RECEIVE_REVIEWS  = 'RECEIVE_REVIEWS';
 
-export const requestReviews = (app) => ({
+export const requestReviews = () => ({
   type: REQUEST_REVIEWS,
-  app_requested: app,
 });
 
 export const receiveReviews = reviews => ({
@@ -12,10 +11,13 @@ export const receiveReviews = reviews => ({
   receivedAt: Date.now()
 })
 
-/* TODO: Fetch this from a environment variable */
-uri = 'http://localhost:5000/'
-export const fetchReviews = (app) => (dispatch) => {
-  dispatch(requestReviews(app))
+/* TODO:
+  - [] Fetch the main URI from a environment variable;
+  - [] Receive the app name as parameter;
+*/
+uri = 'http://localhost:5000/product-upsell/reviews'
+export const fetchReviews = () => (dispatch) => {
+  dispatch(requestReviews())
   return fetch(uri)
     .then(response => response.json())
     .then(data => dispatch(receiveReviews(data)))
